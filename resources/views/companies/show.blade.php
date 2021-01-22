2149b57f45bdf07ab6d8b474375bc6d8149d8a4d
Dyon Altena <Alte0010@hz.nl>
2021-01-22 17:25:00 +0100

    Changes Companies.show

--- resources/views/companies/show.blade.php
+++ resources/views/companies/show.blade.php
@@ -6,16 +6,8 @@
             <div class="container">
                 <p class="title is-1">{{$company->name}}</p>
                 <p class="subtitle is-3"></p>
-
             </div>
-
-    <a href="/companies/{{$company->id}}/edit" class="button is-primary">Edit Company</a>
-            <form action="/companies/{{$company->id}}?_method=DELETE" method="POST">
-                @csrf
-                <button class="ui red basic button">DELETE</button>
-            </form>
-
-    </section>
+        </div>
 
 
     <section class="section">
@@ -25,13 +17,24 @@
                 <div class="column is-12">
 
                     <div class="content">
-                        <p>Country: {{ $company->country }}</p>
-                        <p>Founded at: {{ $company->founded_at }}</p>
+                        <h4>Country:</h4> {{ $company->country }}
+                        <br><br>
+                        <h4>Founded at: </h4> {{ $company->founded_at }}
+                        <br><br>
+                        <br><br>
+                        <h4>History:</h4> {!! $company->history !!}
 
-                        {!! $company->history !!}
                     </div>
+
+                    <td><br><br>
+                        <a href="/companies/{{$company->id}}/edit" class="button is-primary">Edit Company</a>
+                        <br><br>
+                        <form action="/companies/{{$company->id}}?_method=DELETE" method="POST">
+                            @csrf
+                            <button class="ui red basic button">DELETE</button></form></td>
                 </div>
             </div>
+        </div>
     </section>
 
 @endsection
